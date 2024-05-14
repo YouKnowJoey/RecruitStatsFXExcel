dc5b63bde1557da3223806c792617f8d2ec0df41
YouKnowJoey <YouKnowJoey@icloud.com>
2024-05-14 15:52:49 -0700

    PDF Editor, PDF Test, and testable Excel Spreadsheet for Program. The testable xlsx file is usable and in the correct format for main program.

---
+++ src/test/java/com/mycompany/liaisondash/PDFTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.liaisondash;
+import com.mycompany.liaisondash.PDFEditor;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Joey
+ */
+public class PDFTest {
+   
+    @Test
+    public void testReverseString() {
+        PDFEditor utils = new PDFEditor();
+
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        PDFEditor utils = new PDFEditor();
+
+    }
+}
+
